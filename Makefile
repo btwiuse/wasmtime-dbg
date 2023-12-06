a1429df350227210d811de17d20b6446bca9b284
btwiuse <54848194+btwiuse@users.noreply.github.com>
2023-12-06 03:56:41 +0000

    add bisect

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ zigbuild:
 	cargo zigbuild --release --target aarch64-unknown-linux-musl
 	cp target/x86_64-unknown-linux-musl/release/wasmtime-dbg wasmtime-linux-x86_64-dbg
 	cp target/aarch64-unknown-linux-musl/release/wasmtime-dbg wasmtime-linux-aarch64-dbg
+	cp target/x86_64-unknown-linux-musl/release/mmap-rs mmap-rs.x86_64
+	cp target/aarch64-unknown-linux-musl/release/mmap-rs mmap-rs.aarch64
 
 zigcc:
 	zig cc -target aarch64-linux-musl --static -o mmap.aarch64 mmap.c
--- mmap.rs
+++ mmap.rs
@@ -1,17 +1,30 @@
 use libc::{c_long, mmap, MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, PROT_NONE};
+use std::env;
 use std::io::Error;
 use std::os::raw::{c_int, c_void};
 use std::ptr::null_mut;
 
 fn main() {
+    const LENGTH: usize = 6444598427648;
+        let args: Vec<String> = env::args().collect();
+    let length: usize = if args.len() > 1 {
+        match args[1].parse::<usize>() {
+            Ok(len) => len,
+            Err(_) => {
+                eprintln!("Invalid length specified. Using default value.");
+                LENGTH
+            }
+        }
+    } else {
+        LENGTH
+    };
     let addr: *mut c_void = null_mut();
-    let length: usize = 6444598427648;
     let prot: c_int = PROT_NONE;
     let flags: c_int = MAP_PRIVATE | MAP_ANONYMOUS;
     let fd: c_int = -1;
     let offset: c_long = 0;
 
-    let result = unsafe { mmap(addr, length, prot, flags, fd, offset) };
+    let result = unsafe { mmap(addr, dbg!(length), prot, flags, fd, offset) };
     if result == MAP_FAILED {
         let err = Error::last_os_error();
         eprintln!("mmap failed: {}", err);
